16ba635b3d3a7c46353333ec94e3b68de39e1451
mikehajj <mike.hajj@gmail.com>
2015-03-17 10:47:50 +0200

    removed <b>something</b>, updated anchors behavior in features page. fixed issue of changing pages and not scrolling to top caused by angular anchors.

--- sections/features/controller.js
+++ sections/features/controller.js
@@ -1,26 +1,20 @@
 "use strict";
 var featuresApp = app.components;
 
-featuresApp.controller('FeaturesPageCtrl', ['$scope', '$http', '$location', '$routeParams', '$anchorScroll', '$timeout', '$compile', '$route', function($scope, $http, $location, $routeParams, $anchorScroll, $timeout, $compile, $route) {
+featuresApp.controller('FeaturesPageCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
 	$scope.featureList = [];
 	$http.get('sections/features/data/features.json').success(function(data) {
 		$scope.featureList = data;
-	});
 
-	$scope.scrollToAnchor = function() {
-		if($routeParams && $routeParams.anchor) {
-			$timeout(function() {
-				var old = $location.hash();
-				$location.hash($routeParams.anchor);
-				var padEl = angular.element(document.getElementById($routeParams.anchor));
-				padEl.html("<br/> <br/> <br/>");
-				$compile(padEl.contents())($scope);
-				$anchorScroll();
-				//reset to old to keep any additional routing logic from kicking in
-				$location.hash(old);
-			}, 100, false);
+		if($routeParams && $routeParams.anchor && $routeParams.anchor !== '' && jQuery) {
+			setTimeout(function() {
+
+				var elId = jQuery('#'+ $routeParams.anchor);
+				jQuery("html body").animate({
+					scrollTop: elId.offset().top - 50
+				}, 500);
+
+			}, 500);
 		}
-	};
-
-	$scope.scrollToAnchor();
+	});
 }]);
