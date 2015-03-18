24891cbc7501c5fb53087d7beb9af5e7d8e1659f
mikehajj <mike.hajj@gmail.com>
2015-03-18 17:35:33 +0200

    updates on homepage and template footer

--- sections/home/controller.js
+++ sections/home/controller.js
@@ -10,13 +10,7 @@ homeApp.controller('homePageCtrl', ['$scope', '$http', function($scope, $http) {
 		$scope.fList = data;
 	});
 
-	//$http.get("https://api.github.com/repos/soajs/soajs/commits?client_id=c6ef8804597602e35576&client_secret=3cfd267e548c28e7bad8d581c6eb612af7df0719").success(function(data) {
-	//	var lastCommit = data[0];
-	//	var ago = new Date(lastCommit.commit.author.date);
-	//
-	//	$scope.lastCommit = {
-	//		author: lastCommit.commit.author.name,
-	//		ago: moment([ago.getFullYear(), ago.getMonth(), ago.getDate()]).fromNow()
-	//	};
-	//});
+	$http.get("sections/home/repos.js").success(function(data) {
+		$scope.repos = data;
+	});
 }]);
