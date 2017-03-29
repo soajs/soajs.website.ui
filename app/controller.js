dadaa6648da526934b72a4fe93633375fd7ce00b
elianenassif <nassif.eliane@gmail.com>
2017-03-29 18:11:33 +0300

    initial commit

--- app/controller.js
+++ app/controller.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = angular.module('mainWebsite', ['ui.bootstrap', 'ngRoute', 'ngSanitize']);
+var app = angular.module('mainWebsite', ['ui.bootstrap', 'ngRoute']);
 
 app.config([
 	'$routeProvider',
@@ -87,19 +87,8 @@ app.controller('mainCtrl', ['$scope', '$location', '$routeParams', function ($sc
 			var p = $location.path().split(sp);
 			$scope.currentLocation = p[0];
 		}
-		
+
 	});
-	/*
-	 if($routeParams && $routeParams.anchor && $routeParams.anchor !== '' && jQuery) {
-	 setTimeout(function() {
-
-	 var elId = jQuery('#'+ $routeParams.anchor);
-	 jQuery("html body").animate({
-	 scrollTop: elId.offset().top - 50
-	 }, 500);
-
-	 }, 500);
-	 }*/
 
 	$scope.copyrightYear = "2015";
 	var thisYear = new Date().getFullYear();
@@ -114,146 +103,9 @@ app.directive('topMenu', function () {
 	}
 });
 
-app.filter('toTrustedHtml', ['$sce', function ($sce) {
-	return function (text) {
-		return $sce.trustAsHtml(text);
-	};
-}]);
 
 app.filter('trustAsResourceUrl', ['$sce', function ($sce) {
 	return function (val) {
 		return $sce.trustAsResourceUrl(val);
 	};
 }]);
-
-app.service('loadFileContent', ['$http', '$timeout', '$compile', function ($http, $timeout, $compile) {
-	return function (context, path, elId) {
-		var att = "jsCode_" + elId;
-		$http.get(path).success(function (data) {
-			context[att] = data.trim();
-			var htmlData = '<pre id="' + att + '"><div style="text-align:right;"><a href="" onclick="collapseExpand(this);">[ - ]</a></div><code class="javascript">' + context[att] + '</code></pre>';
-			var el = angular.element(document.getElementById(elId));
-			el.html(htmlData);
-			$compile(el.contents())(context);
-
-			$timeout(function () {
-				renderCodeSnippets(att);
-			}, 1000);
-
-		}).error(function () {
-			console.log('Error Loading JS Snippet.');
-		});
-	}
-}]);
-
-app.service('loadJsonFileContent', ['$http', '$timeout', '$compile', function ($http, $timeout, $compile) {
-	return function (context, path, elId) {
-		var att = "jsonCode_" + elId;
-		$http.get(path).success(function (data) {
-			context[att] = data;
-			context[att] = JSON.stringify(context[att], null, "\t");
-			var htmlData = '<pre id="' + att + '"><div style="text-align:right;"><a href="" onclick="collapseExpand(this);">[ - ]</a></div><code class="json">' + context[att] + '</code></pre>';
-			var el = angular.element(document.getElementById(elId));
-			el.html(htmlData);
-			$compile(el.contents())(context);
-
-			$timeout(function () {
-				renderCodeSnippets(att);
-			}, 1000);
-
-		}).error(function () {
-			console.log('Error Loading JSON Snippet: ' + path);
-		});
-	}
-}]);
-
-app.service('loadHTMLContent', ['$http', '$timeout', '$compile', '$sce', function ($http, $timeout, $compile, $sce) {
-
-	function htmlEntities(str) {
-		return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-	}
-
-	return function (context, path, elId) {
-		var att = "htmlCode_" + elId;
-		$http.get(path).success(function (data) {
-			context[att] = $sce.trustAsHtml(htmlEntities(data));
-			var htmlData = '<pre id="' + att + '"><div style="text-align:right;"><a href="" onclick="collapseExpand(this);">[ - ]</a></div><code class="html" ng-bind-html="' + att + '"></code></pre>';
-			var el = angular.element(document.getElementById(elId));
-			el.html(htmlData);
-			$compile(el.contents())(context);
-
-			$timeout(function () {
-				renderCodeSnippets(att);
-			}, 100);
-
-		}).error(function () {
-			console.log('Error Loading HTML Snippet.');
-		});
-	}
-}]);
-
-app.service('loadBASHContent', ['$http', '$timeout', '$compile', '$sce', function ($http, $timeout, $compile, $sce) {
-
-	function htmlEntities(str) {
-		return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-	}
-
-	return function (context, path, elId) {
-		var att = "htmlCode_" + elId;
-		$http.get(path).success(function (data) {
-			context[att] = $sce.trustAsHtml(htmlEntities(data));
-			var htmlData = '<pre id="' + att + '"><code class="bash" ng-bind-html="' + att + '"></code></pre>';
-			var el = angular.element(document.getElementById(elId));
-			el.html(htmlData);
-			$compile(el.contents())(context);
-
-			$timeout(function () {
-				renderCodeSnippets(att);
-			}, 100);
-
-		}).error(function () {
-			console.log('Error Loading HTML Snippet.');
-		});
-	}
-}]);
-
-function setCodeSnipet(context, snippet) {
-	context.htmlCode = snippet;
-}
-
-var preBasket = [];
-function renderCodeSnippets(preId) {
-	hljs.configure({"tabReplace": "    "});
-	if (!preId) {
-		setTimeout(function () {
-			jQuery('pre code').each(function (i, block) {
-				var parentId = jQuery(block).parent().attr('id');
-				if (preBasket.indexOf(parentId) === -1) {
-					hljs.highlightBlock(block);
-				}
-			});
-		}, 200);
-	}
-	else {
-		preBasket.push(preId);
-		jQuery('pre#' + preId + ' code').each(function (i, block) {
-			hljs.highlightBlock(block);
-		});
-	}
-}
-
-function collapseExpand(el) {
-	var codeEl = jQuery(el).parent().parent().children('code');
-
-	if (jQuery(el).html() == "[ - ]") {
-		codeEl.slideUp();
-		jQuery(el).parent().css("background-color", "#f0f0f0");
-		jQuery(el).html("[ + ]");
-	}
-	else {
-		codeEl.slideDown();
-		jQuery(el).parent().css("background-color", "#ffffff");
-		jQuery(el).html("[ - ]");
-	}
-	return false;
-}
--- sections/home/controller.js
+++ sections/home/controller.js
@@ -1,16 +1,9 @@
 "use strict";
 var homeApp = app.components;
-homeApp.controller('homePageCtrl', ['$scope', '$http', function($scope, $http) {
+homeApp.controller('homePageCtrl', ['$scope', '$http', function ($scope, $http) {
 	$scope.fList = [];
-	$http({
-		method: 'GET',
-		url: 'sections/features/data/features.json',
-		cache: true
-	}).success(function(data) {
-		$scope.fList = data;
-	});
-
-	$http.get("sections/home/repos.json").success(function(data) {
+	
+	$http.get("sections/home/repos.json").success(function (data) {
 		$scope.repos = data;
 	});
 }]);
