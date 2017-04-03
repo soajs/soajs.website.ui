a384cd9eb296e883de853f86903458b1c426ec15
Jamil <jamil.abdallah@gmail.com>
2017-04-03 13:27:08 +0300

    font awesome
    Switching of home page template

--- sections/home/controller.js
+++ sections/home/controller.js
@@ -1,9 +1,10 @@
 "use strict";
 var homeApp = app.components;
 homeApp.controller('homePageCtrl', ['$scope', '$http', function ($scope, $http) {
-	$scope.fList = [];
-	
-	$http.get("sections/home/repos.json").success(function (data) {
-		$scope.repos = data;
-	});
+    $scope.name = "Jamil";
+    $scope.page = {
+        "name": "Home",
+        "bannerClass": "homeTop"
+    };
+
 }]);
