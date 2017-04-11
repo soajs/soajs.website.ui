e9c08436deadf30830e0df480747b5248ee203d4
Jamil <jamil.abdallah@gmail.com>
2017-04-11 10:50:19 +0300

    SCROLLING SIZE BEFORE switcvhing menu

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll >= 390) {
+    if (scroll >= 280) {
         $(".topbar").addClass("scrolling");
         $("img.logo").attr("src", "images/logoo.png");
 
