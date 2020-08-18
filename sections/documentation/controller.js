0e700361ae56a500309176f587a5a68bc5a6c9b1
antoinehage <me@antoinehage.com>
2020-08-18 10:08:37 -0400

    updated documentations

--- sections/documentation/controller.js
+++ sections/documentation/controller.js
@@ -30,14 +30,9 @@ docApp.controller('docPageCtrl', ['$scope', '$timeout', function ($scope, $timeo
 			"link": "https://soajsorg.atlassian.net/wiki/spaces/CON"
 		},
 		{
-			"name": "URAC",
-			"description": "User Registration and Access Control ( URAC ), is a multitenant service that provides REST APIs to work and manage users accounts, groups and access levels for all tenants.",
-			"link": "https://soajsorg.atlassian.net/wiki/spaces/URAC"
-		},
-		{
-			"name": "oAuth",
-			"description": "SOAJS oAuth is a multitenant microservice that provides out of the box implementation of oAuth 2.0 that you can use to secure your cloud.",
-			"link": "https://soajsorg.atlassian.net/wiki/spaces/OAUT"
+			"name": "Microservices",
+			"description": "SOAJS is built out of many microservices combined together to create the products available as: SOAJS Marketplace, Multi Environment Orchestration and Deployment, SOAJS Multi Tenant Authentication and Authorization Gateway, etc ...",
+			"link": "https://soajsorg.atlassian.net/wiki/x/KYApVQ"
 		},
 		{
 			"name": "Middleware",
@@ -54,21 +49,6 @@ docApp.controller('docPageCtrl', ['$scope', '$timeout', function ($scope, $timeo
 			"description": "Speed things up by using the SOAJS composer and organize your daemon's Jobs and microservice's layers (REST - Models - Business Logic).",
 			"link": "https://soajsorg.atlassian.net/wiki/spaces/COMP"
 		},
-		// {
-		// 	"name": "GCS",
-		// 	"description": "Generic Content Service is a ready made product that offers a fully mature and ready to use UI wizard that helps you build any informative content microservice along with it's management UI module at the same time.",
-		// 	"link": "https://soajsorg.atlassian.net/wiki/spaces/GCS"
-		// },
-		// {
-		// 	"name": "Installer",
-		// 	"description": "Deploy Local and Remote clouds using the SOAJS installer. Go live by using the Dashboard and the ready made services.",
-		// 	"link": "https://soajsorg.atlassian.net/wiki/spaces/IN"
-		// },
-		// {
-		// 	"name": "Proxy",
-		// 	"description": "SOAJS Proxy is a microservice that redirects requests between deployed and running environments.",
-		// 	"link": "https://soajsorg.atlassian.net/wiki/spaces/PROX"
-		// },
 		{
 			"name": "Swagger",
 			"description": "SOAJS Swagger Editor allows you to create a microservice by simply writing the documentation of its APIs.",
@@ -181,12 +161,8 @@ docApp.controller('docPageCtrl', ['$scope', '$timeout', function ($scope, $timeo
 					link: "https://soajsorg.atlassian.net/wiki/spaces/CON"
 				},
 				{
-					name: "URAC",
-					link: "https://soajsorg.atlassian.net/wiki/spaces/URAC"
-				},
-				{
-					name: "oAuth",
-					link: "https://soajsorg.atlassian.net/wiki/spaces/OAUT"
+					name: "Microservices",
+					link: "https://soajsorg.atlassian.net/wiki/x/KYApVQ"
 				},
 				{
 					name: "Middleware",
