2948a3267160a88b53ff121b688b92ba94522a1d
antoinehage <me@antoinehage.com>
2020-11-23 14:37:12 -0500

    new feature list

--- sections/home/controller.js
+++ sections/home/controller.js
@@ -11,13 +11,27 @@ homeApp.controller('homePageCtrl', ['$scope', '$http', function ($scope, $http) 
     $scope.$parent.$emit('refreshPageTitle', pageData);
 
 
-    $http.get("sections/home/repos.json").success(function(data) {
-        $scope.repos = data;
-    });
-    
-	$http.get("sections/home/our-product.json").success(function(data) {
-		$scope.ourProductList = data;
+    // $http.get("sections/home/repos.json").success(function(data) {
+    //     $scope.repos = data;
+    // });
+	
+	
+	$http.get("sections/home/api.json").success(function(data) {
+		$scope._api = data;
 	});
+	$http.get("sections/home/cloud.json").success(function(data) {
+		$scope._cloud = data;
+	});
+	$http.get("sections/home/gateway.json").success(function(data) {
+		$scope._gateway = data;
+	});
+	$http.get("sections/home/console.json").success(function(data) {
+		$scope._console = data;
+	});
+	
+	// $http.get("sections/home/our-product.json").success(function(data) {
+	// 	$scope.ourProductList = data;
+	// });
 	
 	$http.get("sections/home/advantages.json").success(function(data) {
 		$scope.ourAdvantagesList = data;
